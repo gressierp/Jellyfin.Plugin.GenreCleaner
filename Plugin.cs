7b2f2c8dc03bfed0dbd1a26a04d5470c2a0db063
gressierp <76651592+gressierp@users.noreply.github.com>
2026-02-13 19:10:33 +0100

    Create Plugin.cs

---
+++ Plugin.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Plugins;
+using MediaBrowser.Model.Serialization;
+using Jellyfin.Plugin.GenreCleaner;
+
+namespace Jellyfin.Plugin.GenreCleaner
+{
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    {
+        public override string Name => "Genre Cleaner";
+        public override Guid Id => Guid.Parse("7a4b2c1d-8e9f-4a3b-b2c1-d8e9f4a3b2c1"); // Ton GUID unique
+
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        public static Plugin? Instance { get; private set; }
+
+        public IEnumerable<PluginPageInfo> GetPages()
+        {
+            return new[]
+            {
+                new PluginPageInfo
+                {
+                    Name = "GenreCleaner",
+                    EmbeddedResourcePath = GetType().Namespace + ".genre_config.html"
+                }
+            };
+        }
+    }
+}
