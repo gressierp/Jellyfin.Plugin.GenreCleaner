02d805ccdb587ea2477608cacc8defdd82a43f36
gressierp <76651592+gressierp@users.noreply.github.com>
2026-02-16 15:53:06 +0100

    Update GenreCleanerEntryPoint.cs

--- GenreCleanerEntryPoint.cs
+++ GenreCleanerEntryPoint.cs
@@ -1,10 +1,12 @@
 using MediaBrowser.Controller.Library;
-using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Controller.Plugins; // C'est celui-ci qui manquait pour IServerEntryPoint
 using MediaBrowser.Controller.Entities;
 using MediaBrowser.Model.Entities;
 using MediaBrowser.Controller.Entities.Movies;
 using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
+using System;
+using System.Linq;
 
 namespace Jellyfin.Plugin.GenreCleaner
 {
@@ -27,13 +29,16 @@ namespace Jellyfin.Plugin.GenreCleaner
 
         private async void OnItemAdded(object? sender, ItemChangeEventArgs e)
         {
+            // Vérification de la config
             if (Plugin.Instance?.Configuration.EnableAutoClean != true) return;
 
+            // On traite les Films et les Séries (Series est dans MediaBrowser.Controller.Entities.TV)
             if (e.Item is Movie || e.Item is MediaBrowser.Controller.Entities.TV.Series)
             {
                 if (Plugin.Instance.CleanGenres(e.Item))
                 {
-                    _logger.LogInformation("GenreCleaner (Auto): Nettoyage des genres pour {0}", e.Item.Name);
+                    _logger.LogInformation("GenreCleaner (Auto): Genres mis à jour pour {0}", e.Item.Name);
+                    // Sauvegarde des métadonnées modifiées
                     await _libraryManager.UpdateItemAsync(e.Item, e.Item, ItemUpdateType.MetadataEdit, default);
                 }
             }
