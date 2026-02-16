4c5e0f0cfdd877f24e698ddd4b93044d3c933b80
gressierp <76651592+gressierp@users.noreply.github.com>
2026-02-16 15:39:50 +0100

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -1,16 +1,19 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using MediaBrowser.Model.Drawing; // Requis pour ImageFormat
+using MediaBrowser.Model.Drawing;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Controller.Library;
 
 namespace Jellyfin.Plugin.GenreCleaner
 {
-    // On garde IHasPluginImage car maintenant le build passe
-    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages
+    public class Plugin : BasePlugin<PluginConfiguration>, IHasWebPages, MediaBrowser.Common.Plugins.IHasPluginImage
     {
         public override string Name => "Genre Cleaner";
         public override Guid Id => Guid.Parse("7a4b2c1d-8e9f-4a3b-b2c1-d8e9f4a3b2c1");
@@ -23,28 +26,65 @@ namespace Jellyfin.Plugin.GenreCleaner
 
         public static Plugin? Instance { get; private set; }
 
-        // CORRECTION ICI : Doit retourner un Stream
         public Stream GetImageResource()
         {
             var type = GetType();
             var resourceName = "Jellyfin.Plugin.GenreCleaner.GenreCleaner.png";
             return type.Assembly.GetManifestResourceStream(resourceName) 
-                   ?? throw new FileNotFoundException($"L'image {resourceName} est introuvable dans les ressources.");
+                   ?? throw new FileNotFoundException($"L'image {resourceName} est introuvable.");
         }
 
-        // CORRECTION ICI : Ne pas utiliser ThumbImageFormat (string) mais ImageFormat (enum)
         public ImageFormat ImageFormat => ImageFormat.Png;
 
         public IEnumerable<PluginPageInfo> GetPages()
         {
-            return new[]
+            return new[] { new PluginPageInfo { Name = "GenreCleaner", EmbeddedResourcePath = GetType().Namespace + ".genre_config.html" } };
+        }
+
+        // --- LE MOTEUR DE NETTOYAGE CENTRALISÉ ---
+        public bool CleanGenres(BaseItem item)
+        {
+            if (item.Genres == null || item.Genres.Length == 0) return false;
+            var config = Configuration;
+            if (string.IsNullOrWhiteSpace(config.Mappings)) return false;
+
+            // Préparation du dictionnaire
+            var mappingRules = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            var lines = config.Mappings.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
             {
-                new PluginPageInfo
+                var parts = line.Split('=');
+                if (parts.Length == 2) mappingRules[parts[0].Trim()] = parts[1].Trim();
+            }
+
+            var newGenresList = new List<string>();
+            bool hasChanged = false;
+
+            foreach (var g in item.Genres)
+            {
+                if (mappingRules.TryGetValue(g, out var newGenre))
                 {
-                    Name = "GenreCleaner",
-                    EmbeddedResourcePath = GetType().Namespace + ".genre_config.html"
+                    newGenresList.Add(newGenre);
+                    hasChanged = true;
                 }
-            };
+                else
+                {
+                    newGenresList.Add(g);
+                }
+            }
+
+            if (hasChanged)
+            {
+                item.Genres = newGenresList.Distinct().ToArray();
+                var lockedFields = item.LockedFields.ToList();
+                if (!lockedFields.Contains(MetadataField.Genres))
+                {
+                    lockedFields.Add(MetadataField.Genres);
+                    item.LockedFields = lockedFields.ToArray();
+                }
+            }
+
+            return hasChanged;
         }
     }
 }
